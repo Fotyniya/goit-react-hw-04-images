15c4f912dbd2e788c07f64d613969b2c278b01f8
Fotyniya <s.kozachenko@ekwo.dp.ua>
2023-03-19 19:40:11 +0200

    useRef

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import axios from 'axios';
 //import toast from 'react-hot-toast';
@@ -7,6 +7,7 @@ import { Loader } from  '../Loader'
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Button } from '../Button/Button';
 import { Gallery } from '../ImageGallery/ImageGallery.styled';
+
 
 const API_KEY = '32817596-3735423159e4b61dcdcaf4a45';
 const BASE_URL = 'https://pixabay.com/api/';
@@ -18,40 +19,18 @@ export const ImageGallery = ({ query }) => {
     const [gallery, setGallery] = useState([]);
     const [totalHits, setTotalHits] = useState(0);
     const [page, setPage] = useState(1);
-
-useEffect(() => {
-    setPage(1);
-    setGallery([]);
-},[query]);
-
-// const fetchGallery = (page) =>  {
-//     //setIsLoading(true);
-    
-//     axios.get(`${BASE_URL}?q=${query}&page='${page}'&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
-//     .then((response) => {
-//         console.log(response)
-//         if (response.data.totalHits === 0) {
-//             setError ("Error! Try again!");
-//             return toast.error(error);
-//         }
-//             setGallery(prevState => [...prevState, response.data.hits])
-//             setTotalHits(response.data.totalHits);
-//     })
-//     .catch(error => {
-//         setError("Error! Try again!");
-//     })
-//     .finally(() => {
-//         setIsLoading(false)
-//     });
-// };
+    let queryRef = useRef('');
+    let pageRef = useRef(1);
 
 useEffect(() => {
     
     if (!query) {
         setIsLoading(false);
         return
-    } else {
-    async function fetchData(){
+    };
+
+    async function fetchData(page){
+        
         setIsLoading(true);
         try {
             const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
@@ -59,19 +38,25 @@ useEffect(() => {
             setGallery(prevState => [...prevState, ...response.data.hits])
             setTotalHits(response.data.totalHits);
         } catch(error) {
-            
             setError(error)
             console.log (error)
         } finally {
-            setIsLoading(false);
+            setIsLoading(false); 
         }
+    };
+    if (queryRef.current !== query) {
+        setGallery([]);
+        pageRef.current = 1;
+        fetchData(pageRef.current);
+    } else {
+        fetchData(pageRef.current); 
     }
-    fetchData();  
-    } 
 }, [error, query, page]);
 
 const loadMore = () => {
     setPage(prevPage => prevPage + 1 );
+    pageRef.current += 1;
+    queryRef.current = query;
   };
 
     return (
